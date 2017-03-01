aef34119d67cbf397e4fe99cd83428395ef03fef
Nick0108 <zidonglin@outlook.com>
2017-03-01 19:20:50 +0800

    feat: Add ch03 including a calculator and 4 number product

---
+++ Chapter03/Ch03Qu05/Ch03Qu05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03Qu05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber, thirdNumber, fourthNumber;
+            string UserName;
+            Console.WriteLine("Give me your name :");
+            UserName = Console.ReadLine();
+            Console.WriteLine($"Welcome {UserName}!");
+            Console.WriteLine("Give me the first Number:");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give me the second Number:");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give me the third Number:");
+            thirdNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give me the fourth Number:");
+            fourthNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The product of {firstNumber}, {secondNumber}, {thirdNumber}, {fourthNumber} is {firstNumber * secondNumber * thirdNumber * fourthNumber}.");
+            Console.ReadKey();
+        }
+    }
+}
