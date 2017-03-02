934b5eabe8b5531eb2b6ff39bc8954e5b8dc12f2
Nick0108 <zidonglin@outlook.com>
2017-03-02 22:40:48 +0800

    feat: Add the chapter4(calculator)

---
+++ Chapter04/Ch04Qu02/Ch04Qu02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace Ch04Qu02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            do
+            {
+                WriteLine("Please give me a number:");
+                firstNumber = ToInt32(ReadLine());
+                WriteLine("please give me another number:");
+                secondNumber = ToInt32(ReadLine());
+                if((firstNumber > 10) && (secondNumber > 10))
+                WriteLine("please make sure the two number must be one of them greater than 10!\nPlease give me two number again!");
+            } while ((firstNumber > 10) && (secondNumber >10));
+            WriteLine($"Your number are {firstNumber} and {secondNumber}");
+            ReadKey();        
+        }
+    }
+}
---
+++ Chapter04/Ch04Qu03/Ch04Qu03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch04Qu03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i+"===");
+                if ((i % 2) == 0)
+                    continue;
+                Console.WriteLine(i);
+                Console.ReadKey();
+            }
+        }
+    }
+}
