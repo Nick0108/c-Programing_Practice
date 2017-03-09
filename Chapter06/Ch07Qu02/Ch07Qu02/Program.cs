8658b39e05e8056b5ccb979f449db4f99afbf401
Nick0108 <zidonglin@outlook.com>
2017-03-09 12:24:48 +0800

    feat:Add the Ch07(debug)

---
+++ Chapter06/Ch07Qu02/Ch07Qu02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch07Qu02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Number4999 = new int[4999];
+            Console.WriteLine(Number4999[3002]);
+            for(int i =0; i < 5000; i++)
+            {
+                Number4999[i] += i;  
+            }
+            //Console.WriteLine(Number4999[3002]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter06/Ch07Qu04/Ch07Qu04/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch07Qu04
+{
+    class Program
+    {
+        enum orientation : byte
+        {
+            North = 1,
+            South = 2,
+            east = 3,
+            west = 4
+        }
+        
+        static void Main(string[] args)
+        {
+            byte myByte = 0;
+            orientation myDirection = orientation.east;
+            Console.WriteLine("Please enter 1-4 to select one direction:");
+            string Userinput = Console.ReadLine();
+            try
+            {
+                //if(myByte==1^ myByte == 2 ^ myByte == 3 ^ myByte == 4)
+                    myByte = Convert.ToByte(Userinput);
+            }
+            catch
+            {
+                Console.WriteLine($"your input {Userinput} is not a viable number!");
+            }
+            finally
+            {
+                myDirection = checked((orientation)myByte);
+                if(myByte==1^ myByte == 2 ^ myByte == 3 ^ myByte == 4)
+                Console.WriteLine(myDirection);
+                else
+                Console.WriteLine($"your input {Userinput} is not round(1-4) number!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
