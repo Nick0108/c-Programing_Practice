2b054c6172ad58919d084af1b5f48b7c06bc8bd4
Nick0108 <zidonglin@outlook.com>
2017-03-16 23:10:34 +0800

    feat ; Add the Chapter9(opp)

---
+++ Chapter09/Ch09Ex01/Ch09Ex01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch09Ex01
+{
+    public abstract class Mybase { }
+    internal class Myclass : Mybase { }
+    public interface IMyBaseInterface { }
+    internal interface IMyBaseInterface2 { }
+    internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2 { }
+    internal sealed class MyComplexClass : Myclass, IMyInterface { }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyComplexClass myObj = new MyComplexClass { };
+            WriteLine(myObj.ToString());
+            ReadKey();
+        }
+    }
+}
