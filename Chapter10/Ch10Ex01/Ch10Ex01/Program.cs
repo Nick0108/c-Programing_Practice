e222d121b242b5a277b1a96cdd15a7bc5845c1b6
Nick0108 <zidonglin@outlook.com>
2017-03-19 22:33:50 +0800

    feat : Add the Chapter10(class and build)

---
+++ Chapter10/Ch10Ex01/Ch10Ex01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch10Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Creating object myObj ... ");
+            MyClass myObj = new MyClass("My Object");
+            WriteLine("myObject created.");
+            for(int i = -1; i <= 0; i++)
+            {
+                try
+                {
+                    WriteLine($"\nAttempting to assign {i} to myObj.Val ...");
+                    myObj.Val = i;
+                    WriteLine($"Value{myObj.Val} assigned to myObj.Val.");
+                }
+                catch (Exception e)
+                {
+                    WriteLine($"Exceptiom{e.GetType().FullName} thrown.");
+                    WriteLine($"Message:\n\"{e.Message}\"");
+                }
+            }
+            WriteLine("\nOutputting myObj.tostring()...");
+            WriteLine(myObj.ToString());
+            WriteLine("myObj.ToString() Output.");
+            
+            WriteLine("\nmyDoubleIntProp = 5 ...");
+            WriteLine($"Getting myDoubleIntProp of 5 is {myObj.myDoubledIntProp}");
+
+            ReadKey();
+        }
+    }
+}
