3e657de4f15c94be725b7253da716f2fdf7ad4e9
Nick0108 <zidonglin@outlook.com>
2017-03-20 16:55:54 +0800

    feat: Add the projects of random cards

---
+++ Chapter10/Ch10Ex02/Ch10Ex02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch10Ex02
+{
+    public class ClassA
+    {
+        private int state = -1;
+        public int State
+        {
+            get
+            {
+                return state;
+            }
+        }
+        public class classB
+        {
+            public void SetPrivateState(ClassA target,int newState)
+            {
+                target.state = newState;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA myobject = new ClassA();
+            WriteLine($"myObject.State = {myobject.State}");
+            ClassA.classB myOtherObject = new ClassA.classB();
+            myOtherObject.SetPrivateState(myobject,999);
+            WriteLine($"myObject.State = {myobject.State}");
+            ReadKey();
+        }
+    }
+}
