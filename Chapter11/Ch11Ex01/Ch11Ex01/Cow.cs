b57b0091c33d0197b11dd1024ab37cdbc164053c
Nick0108 <zidonglin@outlook.com>
2017-03-29 12:01:15 +0800

    feat:Add the chap11ex01(Arraylist)

---
+++ Chapter11/Ch11Ex01/Ch11Ex01/Cow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch11Ex01
+{
+    public class Cow : Animal
+    {
+        public void milk()
+        {
+            WriteLine($"{name} has been milked.");
+        }
+        public Cow(string newName) : base(newName) { }
+    }
+}
