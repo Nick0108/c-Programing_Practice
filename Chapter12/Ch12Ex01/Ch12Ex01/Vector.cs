fd0b9ef58b04e8405a70d040c22dfbd68294cd72
Nick0108 <zidonglin@outlook.com>
2017-04-01 23:16:32 +0800

    feat: Add the Ch12(generic)

---
+++ Chapter12/Ch12Ex01/Ch12Ex01/Vector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Ch12Ex01
+{
+    class Vector
+    {
+        public double? R = null;
+        public double? Theta = null;
+        public double? ThetaRadians
+        {
+            get { return (Theta * Math.PI / 180.0); }
+        }
+    }
+}
