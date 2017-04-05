2e4069175b26cd5f499e05e00738e693c3deb987
Nick0108 <zidonglin@outlook.com>
2017-04-05 23:07:28 +0800

    feat: Add the Ch12(vector)

--- Chapter12/Ch12Ex01/Ch12Ex01/Vector.cs
+++ Chapter12/Ch12Ex01/Ch12Ex01/Vector.cs
@@ -15,5 +15,45 @@ namespace Ch12Ex01
         {
             get { return (Theta * Math.PI / 180.0); }
         }
+        public Vector(double? r, double? theta)
+        {
+            if(r < 0)
+            {
+                r = -r;
+                theta += 180;
+            }
+            theta = theta % 360;
+            R = r;
+            Theta = theta;
+        }
+        public static Vector operator + (Vector op1, Vector op2)
+        {
+            try
+            {
+                double newX = op1.R.Value * Sin(op1.ThetaRadians.Value) + op2.R.Value * Sin(op2.ThetaRadians.Value);
+                double newY = op1.R.Value * Cos(op2.ThetaRadians.Value) + op2.R.Value * Cos(op2.ThetaRadians.Value);
+                double newR = Sqrt(newX * newX + newY * newY);
+                double newTheta = Atan2(newX, newY) * 180.0 / PI;
+                return new Vector(newR, newTheta);
+            }
+            catch
+            {
+                return new Vector(null, null);
+            }
+        }
+        public static Vector operator - (Vector op1)
+        {
+            return new Vector(-op1.R, op1.Theta);
+        }
+        public static Vector operator - (Vector op1, Vector op2)
+        {
+            return op1 + (-op2);
+        }
+        public override string ToString()
+        {
+            string rString = R.HasValue ? R.ToString() : "null";
+            string thetaString = Theta.HasValue ? Theta.ToString() : "null";
+            return string.Format($"({rString}, {thetaString})");
+        }
     }
 }
