64c03408d2f334f1c1a80c3830b97686608766bc
Nick0108 <zidonglin@outlook.com>
2017-04-18 22:54:11 +0800

    Feat: Add the Ch12(event)

---
+++ Chapter13/Ch13Ex02/Ch13Ex02/Display.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch13Ex02
+{
+    public class Display
+    {
+        public void DisplayMessage(string message) => Console.WriteLine($"Message arrived: {message}");
+    }
+}
